1eb346aaf93ad131df6bb5737460b0f913189d87
ben-m-m <mainaben23@gmail.com>
2023-10-06 19:45:47 +0300

    float annotated

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Adds two float numbers and returns their sum as a float.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of 'a' and 'b' as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """sum"""
+    return a + b
