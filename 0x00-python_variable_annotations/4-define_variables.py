290293d5a4db1217f66677b152f69612569a90e0
ben-m-m <mainaben23@gmail.com>
2023-10-09 14:34:27 +0300

    define vars

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -6,7 +6,7 @@ pi, a float with a value of 3.14
 i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
-a:  int = 1
+a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
