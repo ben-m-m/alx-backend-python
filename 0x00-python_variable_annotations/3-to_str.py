093bdf85c8d82b1dcdb33d05e3fb5926ba15d5dd
ben-m-m <mainaben23@gmail.com>
2023-10-09 13:47:02 +0300

    float to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function to_str that takes a float n as argument and
+returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns n in str representation
+    """
+    return str(n)
